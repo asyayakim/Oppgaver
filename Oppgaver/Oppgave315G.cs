47548760ed46084401c47a7b25b12112492eeeec
asyayakim <asyayakimova@yahoo.com>
2024-11-13 16:11:50 +0100

    Refactoring
    Not sure that the code is looks better at least everything is working

--- Oppgaver/Oppgave315G.cs
+++ Oppgaver/Oppgave315G.cs
@@ -5,25 +5,38 @@ public class Oppgave315G
     public void Run()
     {
         var range = 250;
-        var counts = new int[range];
         string text = "something";
         while (!string.IsNullOrWhiteSpace(text))
         {
-            text = Console.ReadLine();
-            foreach (var character in text.ToLower())
-            {
-                counts[(int)character]++;
-            }
+            var counts = CountChar(text, range);
             int totalCharacters = counts.Sum();
-            for (var i = 0; i < range; i++)
-            {
-                if (counts[i] > 0)
-                {
-                    var character = (char)i;
-                    double persentage = (double)counts[i] / totalCharacters * 100;
-                    Console.WriteLine(character + " - " + counts[i] + " - " + persentage + "%");
-                }
-            }
+            DisplayChar(counts, totalCharacters, range);
+
         }
+    }
+
+    public static int[] CountChar(string text, int range)
+    {
+        text = Console.ReadLine();
+        int[] counts = new int[range];
+        foreach (var character in text.ToLower())
+        {
+            counts[(int)character]++;
+        }
+
+        return counts;
+    }
+    
+    public static void DisplayChar(int[] counts, int totalCharacters, int range)
+    {
+        for (var i = 0; i < range; i++)
+        {
+            if (counts[i] > 0)
+            {
+                var character = (char)i;
+                double persentage = (double)counts[i] / totalCharacters * 100;
+                Console.WriteLine(character + " - " + counts[i] + " - " + persentage + "%");
+            }
+        } 
     }
 }
