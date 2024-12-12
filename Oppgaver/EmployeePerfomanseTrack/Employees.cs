f75128a9865667670ec84338199429a0a764af49
asyayakim <asyayakimova@yahoo.com>
2024-12-12 16:25:58 +0100

    Added methods

--- Oppgaver/EmployeePerfomanseTrack/Employees.cs
+++ Oppgaver/EmployeePerfomanseTrack/Employees.cs
@@ -8,7 +8,7 @@ namespace Oppgaver.EmployeePerfomanseTrack
             Id = id;
         }
 
-        public static string Id { get; set; }
+        public string Id { get; set; }
 
         public string Name { get; set; }
     }
--- Oppgaver/EmployeePerfomanseTrack/PerfomanceTraker.cs
+++ Oppgaver/EmployeePerfomanseTrack/PerfomanceTraker.cs
@@ -10,11 +10,29 @@ namespace Oppgaver.EmployeePerfomanseTrack
             var employeeScore = new EmployeeScore(id, score, dateTime);
             _listOfEmployees.Add(employeeScore);
         }
-
-        public void FindHighestScore()
+        public int FindHighestScore()
         {
             var score = _listOfEmployees.OrderByDescending(e => e.Score);
-            Console.WriteLine($"Highest - " + score.First().Score + " Lowest - " + score.Last().Score);
+            return score.First().Score;
+            
+        }
+        public int FindLowestScore()
+        {
+            var score = _listOfEmployees.OrderByDescending(e => e.Score);
+            return score.Last().Score;
+        }
+        public void FindEmployeeNameByScore(int lowest, List<Employees> employees)
+        {
+            var id = _listOfEmployees.Find(e => e.Score == lowest)?.Id;
+            
+            foreach (var employee in employees)
+            {
+                if (employee.Id == id)
+                {
+                    var nameScore = employee.Name;
+                    Console.WriteLine(nameScore);
+                }
+            }
         }
     }
 }
